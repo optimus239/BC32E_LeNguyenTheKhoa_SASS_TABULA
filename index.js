008e3cff811f7e4455cb993312d1553d0679b0a9
khoa lnt <khoa201191@gmail.com>
2022-08-01 02:43:12 +0700

    Fix header

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ getElement("btntoggle").onclick = () => {
   getElement("body").classList.toggle("no-scroll");
 };
 
+let querySelect = (i) => {
+  return document.querySelector(i);
+};
+
+// location.reload(){
+
+// }
+
 window.onscroll = () => {
   scrollFuction();
 };
@@ -19,20 +27,8 @@ function scrollFuction() {
     document.body.scrollTop > 300 ||
     document.documentElement.scrollTop > 300
   ) {
-    getElement("navbar-header").style.height = "48px";
-    getElement("navbar-header").style.backgroundColor = "#fff";
-    getElement("headerPage").style.backgroundColor = "#fff";
-    getElement("logo").style.display = "none";
-    getElement("navbarUl").style.marginLeft = "355px";
-    getElement("circle-about").style.top = "-6px";
-    getElement("phone-header").style.padding = "10px 20px";
+    querySelect("header").className = `header-sub header-open`;
   } else {
-    getElement("navbar-header").style.height = "80px";
-    getElement("navbar-header").style.backgroundColor = "transparent";
-    getElement("headerPage").style.backgroundColor = "transparent";
-    getElement("logo").style.display = "inline-block";
-    getElement("navbarUl").style.marginLeft = "275px";
-    getElement("circle-about").style.top = "26px";
-    getElement("phone-header").style.padding = "14px 20px";
+    querySelect("header").className = `header`;
   }
 }
